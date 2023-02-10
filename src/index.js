82a8ce50e54d4b552003bbc2f8c3963f9db6273c
Uladimir Hrydzin <hrydzin@gmail.com>
2023-02-10 02:28:14 +0100

    feat: add solution of this task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === [] || matrix == null){
+    return [];
+ } else {
+ matrix.map(mat => (matrix.indexOf(mat)%2) ? mat.reverse() : mat);
+} 
+ // for(let mat of matrix) {
+//     if  {
+//       return mat.reverse();
+//     } else {
+//      return  mat;
+//     }
+// })          
+return matrix.flat();
 }
+// towelSort(matrix);
